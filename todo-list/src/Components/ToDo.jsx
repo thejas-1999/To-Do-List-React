4d7f9201001f1a7af3ca1a2efb064b940f7f9666
thejas-1999 <thejask074@gmail.com>
2024-02-08 09:59:10 +0530

    Add a subheading with the current date

--- todo-list/src/Components/ToDo.jsx
+++ todo-list/src/Components/ToDo.jsx
@@ -1,9 +1,27 @@
 const ToDo = () => {
+  const currentDate = new Date();
+  const dayOfWeek = currentDate.getDay();
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDay = daysOfWeek[dayOfWeek];
+
   return (
     <div>
       <div className="app">
         <div className="main-heading">
           <h1>To-Do List</h1>
+        </div>
+
+        <div className="sub-heading">
+          <br />
+          <h2>Whoop,It's {currentDay} 🌝 ☕</h2>
         </div>
       </div>
     </div>
